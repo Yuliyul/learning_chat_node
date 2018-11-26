25f8add36bc884ec8ad848094d0dc76b3f39383b
Yuliyul <ykhazran@gmail.com>
2018-11-26 23:05:19 +0200

    socket

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const path = require('path');
 const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 console.log(publicPath);
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIo(server);
+io.on('connection', (socket)=>{
+	console.log('new connection');
+	socket.on('disconnect', ()=>{
+		console.log('client disconnected');
+	});
+});
 app.use(express.static(publicPath));
 app.get('/', (req, resp)=>{
 	resp.render('index.html');
 });
-app.listen(port,()=>{console.log(`Server start on port ${port}`)});
+server.listen(port,()=>{console.log(`Server start on port ${port}`)});
